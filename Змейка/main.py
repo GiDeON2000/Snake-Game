d4808030a41703265ad2fa241b7f1aebb2fd03f9
GiDeON gideonov <gideonmaxima@gmail.com>
2020-08-22 22:09:36 +0300

    Update main.py

--- Змейка/main.py
+++ Змейка/main.py
@@ -28,53 +28,51 @@ clock = pygame.time.Clock()
 snake = [(x, y)]
 
 while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+	snake = [(i + dx, j + dy) for i, j in snake]
 
-    snake = [(i + dx, j + dy) for i, j in snake]
+	keys = pygame.key.get_pressed()
 
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_RIGHT]:
-        dx = BLOCK_SIZE
-        dy = 0
-    if keys[pygame.K_LEFT]:
-        dx = -BLOCK_SIZE
-        dy = 0
-    if keys[pygame.K_DOWN]:
-        dy = BLOCK_SIZE
-        dx = 0
-    if keys[pygame.K_UP]:
-        dy = -BLOCK_SIZE
-        dx = 0
+	if keys[pygame.K_RIGHT]:
+		dx = BLOCK_SIZE
+		dy = 0
+	if keys[pygame.K_LEFT]:
+		dx = -BLOCK_SIZE
+		dy = 0
+	if keys[pygame.K_DOWN]:
+		dy = BLOCK_SIZE
+		dx = 0
+	if keys[pygame.K_UP]:
+		dy = -BLOCK_SIZE
+		dx = 0
         
-    for i in range(len(snake)):
-        if snake[i][0] > size[0]:
-            snake[i] = (0, snake[i][1])
-        if snake[i][0] < 0:
-            snake[i] = (size[0], snake[i][1])
-        if snake[i][1] > size[1]:
-            snake[i] = (snake[i][0], 0)
-        if snake[i][1] < 0:
-            snake[i] = (snake[i][0], size[1])
-
-
+	for i in range(len(snake)):
+		if snake[i][0] > size[0]:
+			snake[i] = (0, snake[i][1])
+		if snake[i][0] < 0:
+			snake[i] = (size[0], snake[i][1])
+		if snake[i][1] > size[1]:
+			snake[i] = (snake[i][0], 0)
+		if snake[i][1] < 0:
+			snake[i] = (snake[i][0], size[1])
+	
 	if snake[0][0] == apple_x and snake[0][1] == apple_y:
-        snake.append((apple_x + dx, apple_y + dy))
+		snake.append((apple_x + dx, apple_y + dy))
+		apple_x = random.randrange(0, size[0], BLOCK_SIZE)
+		apple_y = random.randrange(0, size[1], BLOCK_SIZE)
 
 
-    screen.fill((180, 0, 180))
+	screen.fill((180, 0, 180))
     
-    [pygame.draw.rect(screen, BLACK, (i, j, BLOCK_SIZE, BLOCK_SIZE)) for i, j in snake]
-    pygame.draw.rect(screen, GREEN, (apple_x, apple_y, BLOCK_SIZE, BLOCK_SIZE))
-    
-    
-    if snake[0][0] == apple_x and snake[0][1] == apple_y:
-        snake.append((apple_x, apple_y))
-    print(snake)
-    clock.tick(10)
+	[pygame.draw.rect(screen, BLACK, (i, j, BLOCK_SIZE, BLOCK_SIZE)) for i, j in snake]
+	pygame.draw.rect(screen, WHITE, (snake[-1][0], snake[-1][1], BLOCK_SIZE, BLOCK_SIZE))
+	pygame.draw.rect(screen, GREEN, (apple_x, apple_y, BLOCK_SIZE, BLOCK_SIZE))
+
+	print(snake)
+	clock.tick(10)
 
 
-    pygame.display.flip()
+	pygame.display.flip()
